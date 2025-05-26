d65169d43d17cec5101faddeaafbec3b95ebc47a
yessir <tdevelopeer@gmail.com>
2025-05-26 10:28:49 +0300

    Videos index, show + relation with channel

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    public function index(): Collection
+    {
+        return Video::with('channel')->get();
+    }
+
+    public function show(Video $video): Video
+    {
+        return $video->load('channel');
+    }
+}
