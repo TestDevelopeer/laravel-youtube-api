4c85ce7d89cc4e92de9d580f3430d6ea9976ae89
yessir <tdevelopeer@gmail.com>
2025-05-26 10:09:44 +0300

    Categories index, show

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
-        return [
-            'test'
-        ];
+    public function index(): Collection
+    {
+        return Category::all();
+    }
+
+    public function show(Category $category): Category
+    {
+        return $category;
     }
 }
