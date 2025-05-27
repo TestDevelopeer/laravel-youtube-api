862660015b95c831cb9f815dfd457f2981d45e4c
yessir <tdevelopeer@gmail.com>
2025-05-27 15:27:51 +0300

    Factory status for the time period

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -4,8 +4,6 @@ namespace Database\Factories;
 
 use App\Enums\Period;
 use App\Models\Channel;
-use App\Models\Video;
-use DateTime;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Random\RandomException;
 
@@ -22,28 +20,22 @@ class VideoFactory extends Factory
      */
     public function definition(): array
     {
-//        $fakeDate = $this->faker->dateTimeThisYear();
-
-        $fakeDate = $this->fakeDate();
-
         return [
             'title' => ucfirst($this->faker->words(random_int(1, 4), true)),
             'description' => $this->faker->paragraph(),
             'channel_id' => Channel::inRandomOrder()->first(),
-            'created_at' => $fakeDate,
-            'updated_at' => $fakeDate,
         ];
     }
 
-    private function fakeDate(): DateTime
+    public function last(Period $period): VideoFactory|Factory
     {
-        $period = $this->faker->randomElement(['year', 'month', 'week', 'day', 'hour']);
+        return $this->state(function () use ($period) {
+            $fakeDate = $this->faker->dateTimeBetween("-1 $period->value");
 
-        return $this->faker->dateTimeBetween("-1 $period");
-    }
-
-    public function last(Period $period): Video
-    {
-        
+            return [
+                'created_at' => $fakeDate,
+                'updated_at' => $fakeDate,
+            ];
+        });
     }
 }
--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -2,16 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Period;
 use App\Models\Video;
 use Illuminate\Database\Seeder;
+use Random\RandomException;
 
 class VideoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * @throws RandomException
      */
     public function run(): void
     {
-        Video::factory(100)->create();
+        collect(Period::cases())
+            ->each(fn(Period $period) => Video::factory(random_int(2, 20))->last($period)->create());
     }
 }
