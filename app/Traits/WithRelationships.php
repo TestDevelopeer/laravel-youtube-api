ad97ecc890f9d2c258a1287b4d8c27ff2d0f781d
yessir <tdevelopeer@gmail.com>
2025-05-28 14:52:22 +0300

    Validation of nested relations in trait

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -2,12 +2,24 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Arr;
-
 trait WithRelationships
 {
-    public function scopeWithRelationships($query, array|string $with)
+    public function scopeWithRelationships($query, array|string $relationshipsRequest)
     {
-        return $query->with(array_intersect(Arr::wrap($with), static::$relationships ?? []));
+        $validRelationships = collect($relationshipsRequest)
+            ->map(fn(string $relationship): array => explode('.', $relationship))
+            ->filter(function (array $relationships) {
+                return collect($relationships)->reduce(function ($model, $relationship) {
+                    if ($model && method_exists($model, $relationship) && in_array($relationship, $model::$relationships, true)) {
+                        return $model->$relationship()->getRelated();
+                    }
+
+                    return null;
+                }, new static);
+            })
+            ->map(fn(array $relationships): string => implode('.', $relationships))
+            ->all();
+
+        return $query->with($validRelationships);
     }
 }
