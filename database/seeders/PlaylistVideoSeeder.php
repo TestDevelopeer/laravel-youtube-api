160ee0cc50ab953863bcb4dcab99543fdb694f75
yessir <tdevelopeer@gmail.com>
2025-05-28 11:45:14 +0300

    Refactoring PlaylistVideoSeeder.php

--- database/seeders/PlaylistVideoSeeder.php
+++ database/seeders/PlaylistVideoSeeder.php
@@ -2,12 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Channel;
 use App\Models\Playlist;
-use App\Models\Video;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
-use Random\RandomException;
 
 class PlaylistVideoSeeder extends Seeder
 {
@@ -16,29 +14,18 @@ class PlaylistVideoSeeder extends Seeder
      */
     public function run(): void
     {
-        $playlistIds = Playlist::pluck('id');
-        $videoIds = Video::pluck('id');
+        Playlist::with(['channel.videos'])
+            ->get()
+            ->each(
+                fn(Playlist $playlist) => $playlist->videos()->saveMany($this->randomVideosFrom($playlist->channel))
+            );
+    }
 
-        $playlistVideos = $playlistIds->flatMap(
-            fn(int $id) => $this->playlistVideos($id, $this->randomVideoIds($videoIds))
+    private function randomVideosFrom(Channel $channel): Collection
+    {
+        return $channel->videos->whenEmpty(
+            fn() => collect(),
+            fn(Collection $videos) => $videos->random(random_int(1, $videos->count()))
         );
-
-        DB::table('playlist_video')->insert($playlistVideos->all());
-    }
-
-    public function playlistVideos(int $playlistId, Collection $videoIds): Collection
-    {
-        return $videoIds->map(fn(int $id) => [
-            'playlist_id' => $playlistId,
-            'video_id' => $id,
-        ]);
-    }
-
-    /**
-     * @throws RandomException
-     */
-    private function randomVideoIds(Collection $ids): Collection
-    {
-        return $ids->random(random_int(1, count($ids)));
     }
 }
